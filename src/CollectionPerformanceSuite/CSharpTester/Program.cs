02dbb8d13c7a4bad276da8bda9c4e726c9745fdc
faibz <louiswrwright@outlook.com>
2018-11-19 00:07:47 +0000

    added initial solution and project.

---
+++ src/CollectionPerformanceSuite/CSharpTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
