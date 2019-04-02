d25028863732618243ee74320fb7b7d713a112c4
faibz <louiswrwright@outlook.com>
2019-04-03 00:25:07 +0100

    Maybe I should have excluded the .idea folder too... Java tests are here. Only primitive for now, but Small and Large Object tests coming soon.

---
+++ src/Java/JavaTester/src/main/java/tester/java/Objects/LargeObject.java
@@ -0,0 +1,10 @@
+package tester.java.Objects;
+
+public class LargeObject {
+    private SmallObject objectProperty1 = new SmallObject();
+    private SmallObject objectProperty2 = new SmallObject();
+    private SmallObject objectProperty3 = new SmallObject();
+    private SmallObject objectProperty4 = new SmallObject();
+    private SmallObject objectProperty5 = new SmallObject();
+    private int intProperty = 0;
+}
---
+++ src/Java/JavaTester/src/main/java/tester/java/Objects/SmallObject.java
@@ -0,0 +1,6 @@
+package tester.java.Objects;
+
+public class SmallObject {
+    private int intProperty = 0;
+    private char charProperty = 'a';
+}
